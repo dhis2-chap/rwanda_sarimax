a9bc2c91763f9887a09a67e4592a86d021e6e75f
Ivar Grytten <ivar.grytten@gmail.com>
2025-08-26 11:36:12 +0200

    Initial partly integration with chap, under work

--- predict.py
+++ predict.py
@@ -4,14 +4,27 @@ import joblib
 import pandas as pd
 
 def predict(model_fn, historic_data_fn, future_climatedata_fn, predictions_fn):
+    # get all unique districts from historic data
     df = pd.read_csv(future_climatedata_fn)
-    X = df[['rainfall', 'mean_temperature']]
-    model = joblib.load(model_fn)
+    districts = pd.read_csv(historic_data_fn)['location'].unique()
 
-    y_pred = model.predict(X)
-    df['sample_0'] = y_pred
-    df.to_csv(predictions_fn, index=False)
-    print("Predictions: ", y_pred)
+    final_predictions_df = pd.DataFrame()
+
+    for district in districts:
+        model_file_name = model_fn + "_" + district + ".bin"
+        model = joblib.load(model_file_name)
+        future_data_for_district = df[df['location'] == district]
+        print(f"Loaded model for district {district} from {model_file_name}")
+        predictions = model.forecast(steps=len(future_data_for_district), exog=future_data_for_district[['rainfall', 'mean_temperature']])
+        samples_0 = predictions
+        #predictions = pd.Series(predictions, index=test_data.index)
+
+        # put future data for district into final_predictions_df with a new column 'sample_0' containing the predictions
+        future_data_for_district['sample_0'] = samples_0
+        final_predictions_df = pd.concat([final_predictions_df, future_data_for_district])
+
+    final_predictions_df.to_csv(predictions_fn, index=False)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Predict using the trained model.')
--- train.py
+++ train.py
@@ -1,20 +1,50 @@
 import argparse
-
 import joblib
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+
+
+def train_for_district(district_df):
+    features = ['rainfall', 'mean_temperature']
+    train_exog = district_df[features]
+    Y = district_df['disease_cases']
+    Y = Y.fillna(0)  # set NaNs to zero (not a good solution, just for the example to work)
+    
+
+    # Handle missing values if any (forward fill as an example)
+    train_exog = train_exog.fillna(method='ffill')
+
+    my_order = (0, 1, 0)
+    my_seasonal_order = (1, 0, 1, 12)
+
+    # Define SARIMAX model with exogenous variables
+    model = SARIMAX(
+        Y, 
+        exog=train_exog,
+        order=my_order, 
+        seasonal_order=my_seasonal_order
+    )
+
+    # Fit the model
+    model_fit = model.fit()
+    return model_fit
 
 
 def train(csv_fn, model_fn):
     df = pd.read_csv(csv_fn)
-    features = ['rainfall', 'mean_temperature']
-    X = df[features]
-    Y = df['disease_cases']
-    Y = Y.fillna(0)  # set NaNs to zero (not a good solution, just for the example to work)
-    model = LinearRegression()
-    model.fit(X, Y)
+
+    # split df into one df per distinct location
+    models = {}
+    for district in df['location'].unique():
+        print("Training for district: ", district)
+        district_df = df[df['location'] == district]
+        model = train_for_district(district_df)
+        models[district] = model
+    
+        model_file_name = "model_" + district + ".bin"
+        joblib.dump(model, model_file_name)
+    
     joblib.dump(model, model_fn)
-    print("Train - model coefficients: ", list(zip(features,model.coef_)))
 
 
 if __name__ == "__main__":
